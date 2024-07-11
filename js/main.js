e44ba79e9cf1e94a756cfa784599f0dc7eeae572
YanaNaumova <naumovayana87@gmail.com>
2024-07-11 21:25:54 +0200

    add validation

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@ const inputAdress = document.querySelector(".adress");
 const inputPrise = document.querySelector(".prise");
 const inputRecipient = document.querySelector(".recipient");
 
+const labelAdress = document.querySelector(".labelAdress");
+const labelPreis = document.querySelector(".labelPreis");
+const labelRecipient = document.querySelector(".labelRecipient");
+
+const errorAdress = document.createElement("p");
+const errorPreise = document.createElement("p");
+const errorRecipient = document.createElement("p");
+
+const message = document.createElement("p");
+
 const cards = document.querySelector(".cards");
 
 function createNotification(classNameEl, h2Text, pText) {
@@ -63,15 +73,30 @@ function closedNotification(
 
 order.addEventListener("submit", (event) => {
   event.preventDefault();
-  createCard();
-  createNotification(
-    "successNotification",
-    "Заказ создан",
-    "Ожидайте дальнейшей информации"
-  );
-  inputAdress.value = "";
-  inputPrise.value = "";
-  inputRecipient.value = "";
+  order.append(message);
+  if (
+    inputAdress.value !== "" &&
+    inputPrise.value !== "" &&
+    inputRecipient.value !== "" &&
+    errorAdress.textContent === "" &&
+    errorPreise.textContent === "" &&
+    errorRecipient.textContent === ""
+  ) {
+    createCard();
+    createNotification(
+      "successNotification",
+      "Заказ создан",
+      "Ожидайте дальнейшей информации"
+    );
+    inputAdress.value = "";
+    inputPrise.value = "";
+    inputRecipient.value = "";
+    message.style.color = "green";
+    message.textContent = "Ваш заказ успешно отправлен";
+  } else {
+    message.style.color = "red";
+    message.textContent = "Пожалуйста заполните правильно все поля";
+  }
 });
 
 function createCard() {
@@ -142,4 +167,65 @@ function showNotification(btn1, btn2, btn3, statusElement) {
   });
 }
 
-//валидация - адрес - с большой буквы,латиница, минимальная цена максимальная цена только числа, получатель без спец символов и чисел
+function validationAdress() {
+  labelAdress.append(errorAdress);
+  if (inputAdress.value === "") {
+    errorAdress.textContent = "";
+    errorAdress.remove();
+  }
+  if (inputAdress.value.length <= 10) {
+    errorAdress.textContent = "Поле адресс не может быть меньше 10 символов";
+  } else if (inputAdress.value[0] !== inputAdress.value[0].toUpperCase()) {
+    errorAdress.textContent = "Первая буква адреса должна быть заглавная";
+  } else {
+    errorAdress.textContent = "";
+    errorAdress.remove();
+  }
+}
+
+inputAdress.addEventListener("input", () => {
+  validationAdress();
+});
+
+function validationPrise() {
+  labelPreis.append(errorPreise);
+  if (inputPrise.value === "") {
+    errorPreise.textContent = "";
+    errorPreise.remove();
+  }
+
+  if (isNaN(Number(inputPrise.value))) {
+    errorPreise.textContent = "Цена должна быть числом";
+  } else if (!isNaN(Number(inputPrise.value)) && inputPrise.value < 2) {
+    errorPreise.textContent = "Цена должна быть больше 2";
+  } else {
+    errorPreise.textContent = "";
+    errorPreise.remove();
+  }
+}
+
+inputPrise.addEventListener("input", () => {
+  validationPrise();
+});
+
+function validationRecipient() {
+  if (inputRecipient.value === "") {
+    errorRecipient.textContent = "";
+    errorRecipient.remove();
+  }
+
+  labelRecipient.append(errorRecipient);
+  const regex = /\d/;
+  if (inputRecipient.value.length <= 2) {
+    errorRecipient.textContent = "Имя не может быть короче 2-х символов";
+  } else if (regex.test(inputRecipient.value)) {
+    errorRecipient.textContent = "Имя получателя не должно содержать цифры";
+  } else {
+    errorRecipient.textContent = "";
+    errorRecipient.remove();
+  }
+}
+
+inputRecipient.addEventListener("input", () => {
+  validationRecipient();
+});
